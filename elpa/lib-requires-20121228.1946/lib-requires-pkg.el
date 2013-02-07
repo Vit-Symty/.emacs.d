4a744eff2a551aa1e6cba12fd9b3424a6f58b6f0
Yusuke AIKO <yusuke.aiko@gmail.com>
2013-02-07 22:53:24 +0900

    Delete ignore files

--- elpa/lib-requires-20121228.1946/lib-requires-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "lib-requires" "20121228.1946" "Commands to list Emacs Lisp library dependencies. [source: wiki]" (quote nil))
--- elpa/yaoddmuse-20120605.2153/yaoddmuse-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "yaoddmuse" "20120605.2153" "Yet another oddmuse for Emacs [source: wiki]" (quote nil))
