e0b0c95417ececa6c891ea0706603a469d2deaa1
yusuke.aiko@gmail.com <yusuke.aiko@gmail.com>
2013-10-03 10:56:18 +0900

    20-sass-mode.el -> 21-sass-mode.el

--- conf/20-sass-mode.el
+++
@@ -1,6 +0,0 @@
-;; Sass mager mode
-(add-to-list 'auto-mode-alist '("\\.scss$" . sass-mode))
-(defun my-sass-mode-common-init ()
-  (setq tab-width 2)
-  )
-(add-hook 'sass-mode-hook 'my-sass-mode-common-init)
